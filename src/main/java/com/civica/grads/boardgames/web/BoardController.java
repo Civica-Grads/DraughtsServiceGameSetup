90b72067d0a09c9915359d54a1085867922979a0
Bruce Mundin <bruce.mundin@ipl.com>
2017-10-26 10:02:33 +0100

    [feature-request] Added Board request.

--- src/main/java/com/civica/grads/boardgames/web/BoardController.java
+++ src/main/java/com/civica/grads/boardgames/web/BoardController.java
@@ -1,7 +1,16 @@
+
 package com.civica.grads.boardgames.web;
 
+import com.civica.grads.boardgames.enums.Difficulty;
 import com.civica.grads.boardgames.exceptions.GameException;
+import com.civica.grads.boardgames.exceptions.GameSetupException;
 import com.civica.grads.boardgames.model.Board;
+import com.civica.grads.boardgames.model.Game;
+import com.civica.grads.boardgames.model.draughts.DraughtsGame;
+import com.civica.grads.boardgames.model.draughts.DraughtsGame.BoardType;
+import com.civica.grads.boardgames.model.player.HumanPlayer;
+import com.civica.grads.boardgames.model.player.Player;
+import com.civica.grads.boardgames.model.player.draughts.DraughtsAIPlayer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,18 +18,31 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class BoardController {
 
-    
-    @Autowired
-    private Board board;
+    //    @Autowired
+    //    private Board board;
 
-    
-    @RequestMapping("/json")
-    Board getBoard() throws GameException {
-        
+    @RequestMapping("/create")
+    Board createBoard(@RequestParam String gameType) throws GameException {
 
-        return board ;
+        return createBoard(BoardType.valueOf(gameType));
     }
+
     
     
+    
+    private Board createBoard(BoardType boardType) throws GameSetupException {
+        return createGame(boardType).getBoard();
+    }
+
+
+
+
+    private Game createGame(BoardType boardType) throws GameSetupException {
+        Player whitePlayer = new HumanPlayer("Ryan");
+        Player blackPlayer = new DraughtsAIPlayer("Hamza", Difficulty.EASY);
+        Player[] players = {whitePlayer, blackPlayer };
+
+        return new DraughtsGame(boardType, players);
+    }
 
 }
